9753579c558166b3a59ce7bfb3e29347451dc7a6
datadata1234 <datashavadz025@gmail.com>
2024-09-19 21:37:23 +0400

    commit

---
+++ Day 04/homework/leson4.py
@@ -0,0 +1,32 @@
+
+# Bug inglisuri enidan rom gadmovtargmnot nisnavs erors
+# bedugg -nisnavs secdomis gamisworeba
+
+
+print(data)
+print("data")
+
+print("24"+12)
+print(24+12)
+
+
+print(236+277)
+print(402-241)
+print(36*21)
+print(745/5)
+print(2421+1712)
+
+ 
+age1 = 12
+age_in_by_3_years = age1 + 3
+print(age1)
+
+
+age2 = 11
+age_in_by_3_years = age2 + 3
+print(age2)
+
+
+print(age1+age2)
+
+
