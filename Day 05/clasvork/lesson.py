e84ada2034258215dc3a74515bdef00d453eb26a
datadata1234 <datashavadz025@gmail.com>
2024-09-23 22:46:24 +0400

    commit

---
+++ Day 05/clasvork/lesson.py
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+name = (input("please enter your name:"))
+
+age = int(input("please enter your age:"))
+
+surname  = (input("please enter your surname:"))
---
+++ Day 05/homevork/lesson.py
@@ -0,0 +1,54 @@
+# number = int(input("enter your float amber:"))
+# int_number = number // 2
+# print(int_number)
+
+
+
+# name = input("please enter your name:")
+
+# age = int(input("please enter your age:"))
+
+# surname  = input("please enter your surname:")
+
+# hobb = input("please enter your hobb:")
+
+# balans =  input("please enter your balans:")
+
+
+
+
+
+# print(15 % 3)
+# print(36 % 4)
+# print(48 % 6)
+# print(56 % 8)
+# print(125 % 5)
+
+# print(42 // 70)
+# print(324 // 27)
+# print(213 // 98)
+# print(423 // 41)
+
+
+
+
+
+
+# number =  int(input("please enter your number"))
+# print(number * 3 - 20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
