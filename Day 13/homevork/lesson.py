c9b9dae27ff1f2740b1903721fecc3d15378d1ab
datadata1234 <datashavadze025@gmail.com>
2024-10-27 20:07:58 +0400

    commit

---
+++ Day 13/homevork/lesson.py
@@ -0,0 +1,20 @@
+# for i in range(0,11):
+#     print(i)
+
+
+
+#  for i in range(50,151):
+#     print(i)
+
+
+
+
+# for i in range(200,500,2):
+#     print(i)
+
+
+
+
+
+# for i in range(0,50,5):
+#     print(i)
