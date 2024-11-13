06639089375c7e4b49735a12dc9cb9b869f46d55
datadata1234 <datashavadze025@gmail.com>
2024-11-13 21:51:59 +0400

    commit

--- Day 03/homework/leson3.py
+++ Day 03/homework/leson3.py
@@ -1,11 +1,10 @@
+age=12
+age_in_by_2_years=age+2
+print(age+2)
 
-# age=12
-# age_in_by_2_years=age+2
-# print(age+2)
 
-
-# age=37  
-# age_in_by_2_years=age+2
+age=37  
+age_in_by_2_years=age+2
 
 print ("360"+"360")
 
