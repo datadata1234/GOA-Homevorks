2f965a04d5d77f5f1766735baacdcaa7c37cb118
datadata1234 <datashavadze025@gmail.com>
2024-11-25 19:26:16 +0400

    commit

---
+++ Day 07/homvork/lesson.py
@@ -0,0 +1,31 @@
+
+# age = 13
+# age_in_by_25_yars = age + 25
+# print(age_in_by_25_yars)
+
+# age = 46
+# age_in_by_25_yars = age + 25
+# print(age_in_by_25_yars)
+
+# age = 46
+# age_in_by_25_yars = age + 25
+# print(age_in_by_25_yars)
+
+
+
+
+
+
+# name = int(input("please enter your name:"))
+
+
+
+
+
+
+
+
+
+# string:  სტრინგია როცა ბჭყალებით მთავრდება
+# intejer: ინტეჯრი არის მთელი რიცხვი
+# float: ფლოათი ათწილადი რიცხვია
